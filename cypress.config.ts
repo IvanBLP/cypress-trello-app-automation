368f2c0b6bae0d61256cd068bbdbbeacbc519af6
Iván Bertón <ivan.berton.lp@gmail.com>
2024-09-22 20:34:49 -0300

    Add config for task

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from "cypress";
 import tasks from './cypress/support/tasks/tasks'
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+//import {createEsbuildPlugin} from '@badeball/cypress-cucumber-preprocessor/dist/subpath-entrypoints/esbuild'
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
 
 export default defineConfig({
 	e2e: {
-		setupNodeEvents(on, config) {
+		async setupNodeEvents(on, config) {
+			await addCucumberPreprocessorPlugin(on, config)
+			on('file:preprocessor',
+				createBundler({
+					plugins: [createEsbuildPlugin(config)]
+				})
+			)
+
 			on('task', tasks)
-			// implement more node event listeners here
+			// implement more node event lsisteners here
+			return config;
 		},
-		baseUrl: "http://localhost:3000"
+		baseUrl: "http://localhost:3000",
+		specPattern: [
+			"**/*.feature",
+			"**/*.spec.cy.ts"
+		]
 	},
+	watchForFileChanges: false
 });
